666145c48aa389a2c870119225554b0fa516a4b4
Gabrielle Andrade Da Silva <105249937+Gbiiandrad@users.noreply.github.com>
2023-06-12 18:01:22 -0300

    ✅

--- project/Date_br.py
+++ project/Date_br.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 class Date_br:
@@ -29,3 +29,8 @@ class Date_br:
     def format_data(self):
         data_formatada = self.momento_cadastro.strftime("%d/%m/%Y  %H:%M")
         return data_formatada
+
+    def tempo_cadastro(self):
+        tempo_cadastro = (datetime.today() + timedelta(days=30)
+                          ) - self.momento_cadastro
+        return tempo_cadastro
