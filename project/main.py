3bf2e060a85131d1785044e7fe31763d79349c04
Gabrielle Andrade Da Silva <105249937+Gbiiandrad@users.noreply.github.com>
2023-06-12 17:43:23 -0300

    🚧

---
+++ project/main.py
@@ -0,0 +1,13 @@
+from datetime import datetime, timedelta
+from Date_br import Date_br
+
+cadastro = Date_br()
+print(cadastro)
+
+
+'''''
+hoje = datetime.today()
+hoje_formatado = hoje.strftime("%d/%m/%Y  %H:%M")
+print(hoje)
+print(hoje_formatado)
+'''
